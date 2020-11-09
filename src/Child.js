828abee14fbb1b04b743b8557d81116cf119a600
mark <markpackham1@gmail.com>
2020-11-09 17:02:19 +0000

    finish off hacky way of getting a Child to manipulate its Parent

--- src/Child.js
+++ src/Child.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Child.css";
 
-export default function Child() {
+export default function Child(props) {
   return (
     <div className="child">
       <h1>Child</h1>
-      <button>Click to change the title</button>
+      <button onClick={() => props.changeWord("Meow")}>
+        Click to change the title
+      </button>
     </div>
   );
 }
--- src/Parent.js
+++ src/Parent.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Child from "./Child";
 import "./Parent.css";
 
 export default function Parent() {
+  // we need to use React Hooks and a function since you can't pass Props from Child to Parent
+  const [word, setWord] = useState("Parent");
+
   return (
     <div className="parent">
-      <h1>Parent</h1>
+      <h1>{word}</h1>
       <p>
         Hack to manipulate Parent via function in Child learned from
         https://www.youtube.com/watch?v=UrpNtB61qyo
       </p>
-      <Child />
+      <Child changeWord={(word) => setWord(word)} />
     </div>
   );
 }
